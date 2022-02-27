d115466e1a2a5907e888ce7909c1086274d501a2
Gustavo <ghp2201@gmail.com>
2022-02-27 10:11:37 -0300

    Implement short url backend + frontend connection

--- src/controller/shortner.go
+++ src/controller/shortner.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -38,30 +39,44 @@ func (c *ShortnerController) List(ctx *gin.Context) {
 	c.controller.RenderWithData(ctx, "list", gin.H{})
 }
 
-func getRandomUrl(host string, tlsConn *tls.ConnectionState) string {
-	scheme := "http"
-	if tlsConn != nil {
-		scheme = "https"
-	}
-
-	rand.Seed(time.Now().UnixMicro())
-	random := strconv.Itoa(rand.Intn(10000))
-
-	shortned := fmt.Sprintf("%s://%s/%s", scheme, host, random)
-
-	return shortned
-}
-
 func (c *ShortnerController) ShortUrl(ctx *gin.Context) {
-	shortned := getRandomUrl(ctx.Request.Host, ctx.Request.TLS)
-
 	var holder Holder
 
 	if err := ctx.BindJSON(&holder); err != nil {
 		ctx.IndentedJSON(http.StatusInternalServerError, gin.H{})
 	}
 
+	if !strings.Contains(holder.Url, "http://") && !strings.Contains(holder.Url, "https://") {
+		ctx.IndentedJSON(http.StatusBadRequest, gin.H{
+			"error": "The url must be valid, containing either http:// or https://",
+		})
+		return
+	}
+
+	shortned := c.getRandomUrl(ctx.Request.Host, ctx.Request.TLS)
 	c.urls[shortned] = holder.Url
 
-	ctx.IndentedJSON(http.StatusCreated, gin.H{})
+	ctx.IndentedJSON(http.StatusCreated, gin.H{
+		"newUrl": shortned,
+	})
+}
+
+func (c *ShortnerController) getRandomUrl(host string, tlsConn *tls.ConnectionState) string {
+	scheme := "http"
+	if tlsConn != nil {
+		scheme = "https"
+	}
+
+	rand.Seed(time.Now().UnixMicro())
+	random := strconv.Itoa(rand.Intn(1000000))
+
+	shortned := fmt.Sprintf("%s://%s/%s", scheme, host, random)
+
+	for _, url := range c.urls {
+		if shortned == url {
+			return c.getRandomUrl(host, tlsConn)
+		}
+	}
+
+	return shortned
 }
